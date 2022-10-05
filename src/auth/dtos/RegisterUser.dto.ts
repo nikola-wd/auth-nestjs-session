679f0ca885a14ae2d9b1178cf1e4abf539b4e6a7
Nikola Ivanov <nikola.ivanov.wd@gmail.com>
2022-10-05 17:34:53 +0200

    Setup initial auth/register route and basic validation for the dto

--- src/auth/dtos/RegisterUser.dto.ts
+++ src/auth/dtos/RegisterUser.dto.ts
@@ -1,7 +1,21 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+// TODO: Implement custom validation that's same as on the frontend
+// for username and password
 export class RegisterUserDto {
+  @IsNotEmpty()
   firstName: string;
+
+  @IsNotEmpty()
   lastName: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
   username: string;
+
+  @IsNotEmpty()
   password: string;
 }
